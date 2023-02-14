49a9c6e5a77b0d1ee08bab6976e990c619b753b7
Nguyễn Văn Thương <thuong.nguyen@deliveree.com>
2023-02-14 22:32:48 +0700

    change primary key type from int to uuid

--- db/sql/langgeneral/V1__init.sql
+++ db/sql/langgeneral/V1__init.sql
@@ -4,48 +4,49 @@
 
 -- 2. DROP DATABASE
 -- DROP DATABASE langex_general;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE Language (
-   langId int PRIMARY KEY,
+   langId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
    name varchar(255)
 );
 
 CREATE TABLE NotiBox (
-   boxId int PRIMARY KEY
+   boxId uuid DEFAULT uuid_generate_v4() PRIMARY KEY
 );
 
 CREATE TABLE NotiType (
-	typeId int PRIMARY KEY,
+	typeId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	typestring varchar(32) NOT NULL,
 	sample varchar(512) NOT NULL
 );
 
 CREATE TABLE Notifications (
-	notiId int PRIMARY KEY,
-	boxId int NOT NULL,
+	notiId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	boxId uuid NOT NULL,
 	FOREIGN KEY(boxId) REFERENCES NotiBox(boxId),
 	text varchar(512) NOT NULL,
 	url varchar(256) NOT NULL,
-	type int NOT NULL,
+	type uuid NOT NULL,
 	FOREIGN KEY(type) REFERENCES NotiType(typeId)
 );
 
 
 
 CREATE TABLE Users (
-	userId int PRIMARY KEY,
+	userId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	longtt decimal(9,6),
 	latt decimal(8,6),
-	native_lang int,
+	native_lang uuid,
 	FOREIGN KEY(native_lang) REFERENCES Language(langId),
-	notiBox int,
+	notiBox uuid,
 	FOREIGN KEY(notiBox) REFERENCES NotiBox(boxId),
 	email varchar(255) NOT NULL,
 	password varchar(128) NOT NULL,
-	first_name varchar(64) NOT NULL,
+	first_name varchar(64),
 	middle_name varchar(64),
-	last_name  varchar(64) NOT NULL,
-	gender varchar(5) NOT NULL,	
+	last_name  varchar(64),
+	gender varchar(5),	
 	introduction varchar(1024),
 	is_tutor bit(1) DEFAULT B'0',
 	is_restrict bit(1) DEFAULT B'0',
@@ -53,62 +54,62 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Messages (
-	messId int PRIMARY KEY,
-	sender int,
+	messId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	sender uuid,
 	FOREIGN KEY(sender) REFERENCES Users(userId),
-	receiver int,
+	receiver uuid,
 	FOREIGN KEY(receiver) REFERENCES Users(userId),
 	text varchar(1024)
 );
 
 CREATE TABLE CorrectMsg (
-	messId int PRIMARY KEY,
+	messId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(messId) REFERENCES Messages(messId),
 	correct_text varchar(1024)
 );
 
 CREATE TABLE ImageMsg (
-	messId int PRIMARY KEY,
+	messId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(messId) REFERENCES Messages(messId)
 );
 
 CREATE TABLE ImageMsgUrl (
-	urlId int PRIMARY KEY,
-	messId int,
+	urlId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	messId uuid,
 	FOREIGN KEY(messId) REFERENCES ImageMsg(messId),
 	url varchar(256)
 );
 
 
 CREATE TABLE AudioMsg (
-	messId int PRIMARY KEY,
+	messId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(messId) REFERENCES Messages(messId),
 	url varchar(256) NOT NULL
 );
 
 CREATE TABLE AudioMsgUrl (
-	urlId int PRIMARY KEY,
-	messId int,
+	urlId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	messId uuid,
 	FOREIGN KEY(messId) REFERENCES AudioMsg(messId),
 	url varchar(256)
 );
 
 
 CREATE TABLE ChatConf (
-	confId int PRIMARY KEY,
+	confId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	isMute bit(1) DEFAULT B'0',
 	isBlock bit(1) DEFAULT B'0',
-	sender int,
+	sender uuid,
 	FOREIGN KEY(sender) REFERENCES Users(userId),
-	receiver int,
+	receiver uuid,
 	FOREIGN KEY(receiver) REFERENCES Users(userId)
 );
 
 
 
 CREATE TABLE Arrangement (
-	arrangeId int PRIMARY KEY,
-	confId int NOT NULL,
+	arrangeId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	confId uuid NOT NULL,
 	FOREIGN KEY(confId) REFERENCES ChatConf(confId),
 	time_started timestamptz, 
 	time_ended timestamptz,
@@ -118,44 +119,44 @@ CREATE TABLE Arrangement (
 
 
 CREATE TABLE VocabGoal (
-	goalId int PRIMARY KEY,
+	goalId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	amount int DEFAULT 0,
 	type varchar(20)
 );
 
 
 CREATE TABLE Relationship (
-	user_1 int,
+	user_1 uuid,
 	FOREIGN KEY(user_1) REFERENCES Users(userId),
-	user_2 int,
+	user_2 uuid,
 	FOREIGN KEY(user_2) REFERENCES Users(userId),
 	PRIMARY KEY(user_1, user_2),
 	type bit(1) NOT NULL
 );
 
 CREATE TABLE Hobby (
-	hobbyId int PRIMARY KEY,
+	hobbyId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	name varchar(128) NOT NULL
 );
 
 CREATE TABLE UserHobby (
-	userId int NOT NULL,
+	userId uuid NOT NULL,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
-	hobbyId int NOT NULL,
+	hobbyId uuid NOT NULL,
 	FOREIGN KEY(hobbyId) REFERENCES Hobby(hobbyId),
 	PRIMARY KEY(userId, hobbyId)
 );
 
 CREATE TABLE TargetLang (
-	userId int NOT NULL,
+	userId uuid NOT NULL,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
-	langId int NOT NULL,
+	langId uuid NOT NULL,
 	FOREIGN KEY(langId) REFERENCES Language(langId),
 	PRIMARY KEY(userId,langId)
 );
 
 CREATE TABLE Groups (
-	groupId int PRIMARY KEY,
+	groupId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	name varchar(128),
 	introduction varchar(1024),
 	requirement varchar(1024),
@@ -165,37 +166,37 @@ CREATE TABLE Groups (
 );
 
 CREATE TABLE Role(
-	roleId int PRIMARY KEY,
+	roleId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	strRole varchar(10) NOT NULL
 );
 
 CREATE TABLE GroupMember (
-	userId int NOT NULL, 
+	userId uuid NOT NULL, 
 	FOREIGN KEY(userId) REFERENCES Users(userId),
-	groupId int NOT NULL,
+	groupId uuid NOT NULL,
 	FOREIGN KEY(groupId) REFERENCES Groups(groupId),
 	PRIMARY KEY(userId, groupId),
-	roleId int NOT NULL,
+	roleId uuid NOT NULL,
 	FOREIGN KEY(roleId) REFERENCES Role(roleId),
 	restrict_level bit(2) DEFAULT B'00',	
 	is_removed bit(1) DEFAULT B'0'
 );
 
 CREATE TABLE JoinGrpReq (
-	userId int,
+	userId uuid,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
-	groupId int,
+	groupId uuid,
 	FOREIGN KEY(groupId) REFERENCES Groups(groupId),
 	PRIMARY KEY(userId, groupId),
 	is_removed bit(1) DEFAULT B'0'
 );
 
 CREATE TABLE Post (
-	postId int PRIMARY KEY,
-	userId int,
+	postId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	userId uuid,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
 	text varchar(4096),
-	langId int,
+	langId uuid,
 	FOREIGN KEY(langId) REFERENCES Language(langId),
 	restrict_bits bit(3) DEFAULT B'000',
 	is_audio bit(1) DEFAULT B'0',
@@ -208,31 +209,31 @@ CREATE TABLE Post (
 );
 
 CREATE TABLE Interaction (
-	interactId int PRIMARY KEY,
+	interactId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	stringCode varchar(16) NOT NULL
 );
 
 CREATE TABLE UserIntPost (
-	postId int NOT NULL,
+	postId uuid NOT NULL,
 	FOREIGN KEY(postId) REFERENCES Post(postId),
-	userId int NOT NULL,
+	userId uuid NOT NULL,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
 	PRIMARY KEY(postId, userId),
-	interact_type int NOT NULL,
+	interact_type uuid NOT NULL,
 	FOREIGN KEY(interact_type) REFERENCES Interaction(interactId)
 );
 
 CREATE TABLE PostReport (
-	postReportId int PRIMARY KEY,
+	postReportId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	reason varchar(256) NOT NULL
 );
 
 CREATE TABLE UserReportPst (
-	postReportId int NOT NULL,
+	postReportId uuid NOT NULL,
 	FOREIGN KEY(postReportId) REFERENCES PostReport(postReportId),
-	postId int NOT NULL,
+	postId uuid NOT NULL,
 	FOREIGN KEY(postId) REFERENCES Post(postId),
-	userId int NOT NULL,
+	userId uuid NOT NULL,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
 	PRIMARY KEY(postReportId,postId,userId),
 	statement varchar(512)
@@ -241,57 +242,57 @@ CREATE TABLE UserReportPst (
 
 
 CREATE TABLE GroupPost (
-	postId int PRIMARY KEY,
+	postId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(postId) REFERENCES Post(postId),
-	groupId int NOT NULL,
+	groupId uuid NOT NULL,
 	FOREIGN KEY(groupId) REFERENCES Groups(groupId),
 	is_qualified bit(1) DEFAULT B'1',
 	is_hidden bit(1) DEFAULT B'0'
 );
 
 CREATE TABLE ImagePost (
-	postId int PRIMARY KEY,
+	postId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(postId) REFERENCES Post(postId),
 	url varchar(256)
 );
 
 CREATE TABLE AudioPost (
-	postId int PRIMARY KEY,
+	postId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(postId) REFERENCES Post(postId),
 	url varchar(256)
 );
 
 CREATE TABLE SharePost (
-	postId int PRIMARY KEY,
+	postId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(postId) REFERENCES Post(postId),
-	sharedpst int NOT NULL,
+	sharedpst uuid NOT NULL,
 	FOREIGN KEY(sharedpst) REFERENCES Post(postId)
 );
 
 CREATE TABLE AudioRoom (
-	roomId int PRIMARY KEY,
-	owner int NOT NULL,
+	roomId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	owner uuid NOT NULL,
 	FOREIGN KEY(owner) REFERENCES Users(userId),
 	is_closed bit(1) DEFAULT B'0'
 );
 
 CREATE TABLE UserInRoom (
-	userId int PRIMARY KEY,
+	userId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
-	roomId int NOT NULL,
+	roomId uuid NOT NULL,
 	FOREIGN KEY(roomId) REFERENCES AudioRoom(roomId)
 );
 
 CREATE TABLE RoomPost (
-	postId int PRIMARY KEY,
+	postId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(postId) REFERENCES Post(postId),
-	roomId int NOT NULL,
+	roomId uuid NOT NULL,
 	FOREIGN KEY(roomId) REFERENCES AudioRoom(roomId)
 );
 
 CREATE TABLE VocabPackage (
-	packageId int PRIMARY KEY,
-	userId int NOT NULL,
+	packageId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	userId uuid NOT NULL,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
 	name varchar(128) NOT NULL,
 	is_public bit(1) DEFAULT B'1',
@@ -301,7 +302,7 @@ CREATE TABLE VocabPackage (
 
 
 CREATE TABLE Vocabulary (
-	packageId int,
+	packageId uuid,
 	FOREIGN KEY(packageId) REFERENCES VocabPackage(packageId),
 	vocabId int NOT NULL,
 	PRIMARY KEY(packageid,vocabId),
@@ -318,8 +319,8 @@ CREATE TABLE Vocabulary (
 );
 
 CREATE TABLE Topic (
-	topicId int PRIMARY KEY,
-	userId int NOT NULL,
+	topicId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	userId uuid NOT NULL,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
 	name varchar(128),
 	is_public bit(1) DEFAULT B'1',
@@ -327,19 +328,19 @@ CREATE TABLE Topic (
 );
 
 CREATE TABLE PostTopic (
-	topicId int NOT NULL,
+	topicId uuid NOT NULL,
 	FOREIGN KEY(topicId) REFERENCES Topic(topicId),
-	postId int NOT NULL,
+	postId uuid NOT NULL,
 	FOREIGN KEY(postId) REFERENCES Post(postId),
 	PRIMARY KEY(topicId,postId),
 	is_removed bit(1) DEFAULT B'0'
 );
 
 CREATE TABLE Comment (
-	commentId int PRIMARY KEY,
-	userId int NOT NULL,
+	commentId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	userId uuid NOT NULL,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
-	postId int NOT NULL,
+	postId uuid NOT NULL,
 	FOREIGN KEY(postId) REFERENCES Post(postId),
 	text varchar(2048),
 	is_image bit(1) DEFAULT B'0',
@@ -349,51 +350,51 @@ CREATE TABLE Comment (
 );
 
 CREATE TABLE AudioCmt (
-	commentId int PRIMARY KEY,
+	commentId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(commentId) REFERENCES Comment(commentId),
 	url varchar(256) NOT NULL
 );
 
 CREATE TABLE ImageCmt (
-	commentId int PRIMARY KEY,
+	commentId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(commentId) REFERENCES Comment(commentId),
 	url varchar(256) NOT NULL
 );
 
 CREATE TABLE CorrectCmt (
-	commentId int PRIMARY KEY,
+	commentId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(commentId) REFERENCES Comment(commentId),
 	correct_text varchar(2048)
 );
 
 CREATE TABLE CmtInteract (
-	userId int NOT NULL,
+	userId uuid NOT NULL,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
-	commentId int NOT NULL,
+	commentId uuid NOT NULL,
 	FOREIGN KEY(commentId) REFERENCES Comment(commentId),
 	PRIMARY KEY(userId, commentId),
-	interact_type int NOT NULL,
+	interact_type uuid NOT NULL,
 	FOREIGN KEY(interact_type) REFERENCES Interaction(interactId)
 );
 
 CREATE TABLE CmtReport (
-	cmtReportId int PRIMARY KEY,
+	cmtReportId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	reason varchar(256) NOT NULL
 );
 
 CREATE TABLE UsrReportCmt (
-	cmtReportId int NOT NULL,
+	cmtReportId uuid NOT NULL,
 	FOREIGN KEY(cmtReportId) REFERENCES CmtReport(cmtReportId),
-	commentId int NOT NULL,
+	commentId uuid NOT NULL,
 	FOREIGN KEY(commentId) REFERENCES Comment(commentId),
-	userId int NOT NULL,
+	userId uuid NOT NULL,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
 	PRIMARY KEY(cmtReportId, userId, commentId),
 	statement varchar(512)
 );
 
 CREATE TABLE Admin (
-	adminId int PRIMARY KEY,
+	adminId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	email varchar(128) NOT NULL,
 	password varchar(128) NOT NULL,
 	first_name varchar(64) NOT NULL,
@@ -401,78 +402,78 @@ CREATE TABLE Admin (
 );
 
 CREATE TABLE Punishment (
-	punishId int PRIMARY KEY,
+	punishId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	is_restrict bit(1) DEFAULT B'0',
 	relapse int DEFAULT 0,
 	type varchar(32)
 );
 
 CREATE TABLE GroupPunish (
-	adminId int NOT NULL,
+	adminId uuid NOT NULL,
 	FOREIGN KEY(adminId) REFERENCES Admin(adminId),
-	groupId int NOT NULL,
+	groupId uuid NOT NULL,
 	FOREIGN KEY(groupId) REFERENCES Groups(groupId),
-	punishId int NOT NULL,
+	punishId uuid NOT NULL,
 	FOREIGN KEY(punishId) REFERENCES Punishment(punishId),
 	PRIMARY KEY(adminId, groupId, punishId)
 );
 
 CREATE TABLE PostPunish (
-	postId int PRIMARY KEY,
+	postId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(postId) REFERENCES Post(postId),
-	adminId int NOT NULL,
+	adminId uuid NOT NULL,
 	FOREIGN KEY(adminId) REFERENCES Admin(adminId),
-	punishId int NOT NULL,
+	punishId uuid NOT NULL,
 	FOREIGN KEY(punishId) REFERENCES Punishment(punishId),
 	userId int
 );
 
 CREATE TABLE CmtPunish (
-	commentId int PRIMARY KEY,
+	commentId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	FOREIGN KEY(commentId) REFERENCES Comment(commentId),
-	adminId int NOT NULL,
+	adminId uuid NOT NULL,
 	FOREIGN KEY(adminId) REFERENCES Admin(adminId),
-	punishId int NOT NULL,
+	punishId uuid NOT NULL,
 	FOREIGN KEY(punishId) REFERENCES Punishment(punishId),
 	userId int
 );
 
 CREATE TABLE UserPunish (
-	adminId int NOT NULL,
+	adminId uuid NOT NULL,
 	FOREIGN KEY(adminId) REFERENCES Admin(adminId),
-	userId int NOT NULL,
+	userId uuid NOT NULL,
 	FOREIGN KEY(userId) REFERENCES Users(userId),
-	punishId int NOT NULL,
+	punishId uuid NOT NULL,
 	FOREIGN KEY(punishId) REFERENCES Punishment(punishId),
 	PRIMARY KEY(adminId, userId, punishId)
 );
 
 CREATE TABLE Restrict (
-	restrictId int PRIMARY KEY,
+	restrictId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	days int,
 	restrict_code varchar(32),
 	description varchar(256)
 );
 
 CREATE TABLE RestrictPunish (
-	punishId int NOT NULL,
+	punishId uuid NOT NULL,
 	FOREIGN KEY(punishId) REFERENCES Punishment(punishId),
-	restrictId int NOT NULL,
+	restrictId uuid NOT NULL,
 	FOREIGN KEY(restrictId) REFERENCES Restrict(restrictId),
 	PRIMARY KEY(punishId, restrictId)
 );
 
 CREATE TABLE GroupOpReq (
-	requestId int PRIMARY KEY,
-	ownerId int NOT NULL,
+	requestId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	ownerId uuid NOT NULL,
 	FOREIGN KEY(ownerId) REFERENCES Users(userId),
 	text varchar(2048),
 	is_qualified bit(1) DEFAULT B'0'
 );
 
 CREATE TABLE TutorReq (
-	requestId int PRIMARY KEY,
-	ownerId int NOT NULL,
+	requestId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	ownerId uuid NOT NULL,
 	FOREIGN KEY(ownerId) REFERENCES Users(userId),
 	text varchar(2048),
 	is_qualified bit(1) DEFAULT B'0'
