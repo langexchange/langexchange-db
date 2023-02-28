1a6732c3fbe41c60362fc8fe07206910dd169238
Nguyễn Văn Thương <nvthuong1508@gmail.com>
2023-02-28 15:33:32 +0700

    drop database and remigrate

--- langgeneral/sql/V5__add_table_videopost.sql
+++ langgeneral/sql/V5__add_table_videopost.sql
@@ -2,7 +2,8 @@ ALTER TABLE Post
 ADD COLUMN is_video bit(1) DEFAULT B'0';
 
 CREATE TABLE VideoPost (
-	postId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	videoPostId uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	postId uuid NOT NULL,
 	FOREIGN KEY(postId) REFERENCES Post(postId),
 	url varchar(256)
 );
