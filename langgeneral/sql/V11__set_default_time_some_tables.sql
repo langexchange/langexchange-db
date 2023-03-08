70bdfe83befbe77adb51bd463e2961b797fd3e6e
Nguyễn Văn Thương <nvthuong1508@gmail.com>
2023-03-08 14:51:16 +0700

    Update V11__set_default_time_some_tables.sql

--- langgeneral/sql/V11__set_default_time_some_tables.sql
+++ langgeneral/sql/V11__set_default_time_some_tables.sql
@@ -1,20 +1,20 @@
 ALTER TABLE Users
 ADD COLUMN created_at timestamp without time zone default (now() at time zone 'utc'),
-ADD COLUMN updated_at timestamp without time zone default (now() at time zone 'utc');
+ADD COLUMN updated_at timestamp without time zone;
 
 ALTER TABLE Post
 ADD COLUMN created_at timestamp without time zone default (now() at time zone 'utc'),
-ADD COLUMN updated_at timestamp without time zone default (now() at time zone 'utc');
+ADD COLUMN updated_at timestamp without time zone;
 
 ALTER TABLE Vocabulary
 ADD COLUMN created_at timestamp without time zone default (now() at time zone 'utc'),
-ADD COLUMN updated_at timestamp without time zone default (now() at time zone 'utc');
+ADD COLUMN updated_at timestamp without time zone;
 
 ALTER TABLE Comment
 ADD COLUMN created_at timestamp without time zone default (now() at time zone 'utc'),
-ADD COLUMN updated_at timestamp without time zone default (now() at time zone 'utc');
+ADD COLUMN updated_at timestamp without time zone;
 
 ALTER TABLE Admin
 ADD COLUMN created_at timestamp without time zone default (now() at time zone 'utc'),
-ADD COLUMN updated_at timestamp without time zone default (now() at time zone 'utc'),
-ADD COLUMN deleted_at timestamp without time zone default (now() at time zone 'utc');
+ADD COLUMN updated_at timestamp without time zone,
+ADD COLUMN deleted_at timestamp without time zone;
